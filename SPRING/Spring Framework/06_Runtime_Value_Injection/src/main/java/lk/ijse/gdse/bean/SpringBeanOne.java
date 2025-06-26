7bf0f4b5933641c7b09071963b683d51b45ef018
sasuniii0 <sasuniwijerathne@gmail.com>
2025-06-26 11:54:00 +0530

    Implement InitializingBean in SpringBeanOne and add name property

--- SPRING/Spring Framework/06_Runtime_Value_Injection/src/main/java/lk/ijse/gdse/bean/SpringBeanOne.java
+++ SPRING/Spring Framework/06_Runtime_Value_Injection/src/main/java/lk/ijse/gdse/bean/SpringBeanOne.java
@@ -1,10 +1,30 @@
 package lk.ijse.gdse.bean;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBeanOne {
-    public SpringBeanOne() {
-        System.out.println("Spring Bean One");
+public class SpringBeanOne implements InitializingBean {
+    @Value("Sasuni-Wijerathne")
+    private String name;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Spring Bean One with name: " + name);
     }
+
+
+   /* @Autowired(required = false)
+    public SpringBeanOne(@Value("Sasuni-Wijerathne") String name,@Value("21") int age,@Value("true") boolean isActive) {
+        System.out.println("Spring Bean One with name: " + name);
+        System.out.println("Spring Bean One with age: " + age);
+        System.out.println("Spring Bean One with isActive: " + isActive);
+    }
+    @Autowired(required = false)
+    public SpringBeanOne(@Value("Sasuni-Wijerathne") String name,@Value("21") int age) {
+        System.out.println("Spring Bean One with name: " + name);
+        System.out.println("Spring Bean One with age: " + age);
+    }*/
 }
