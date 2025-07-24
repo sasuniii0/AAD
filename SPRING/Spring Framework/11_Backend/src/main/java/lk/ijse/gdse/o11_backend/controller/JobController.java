60de7405c969dedd77bb4276d9aaf6e4067e5725
sasuniii0 <sasuniwijerathne@gmail.com>
2025-07-24 10:16:07 +0530

    Implement APIResponse class and update JobController to return standardized responses

--- SPRING/Spring Framework/11_Backend/src/main/java/lk/ijse/gdse/o11_backend/controller/JobController.java
+++ SPRING/Spring Framework/11_Backend/src/main/java/lk/ijse/gdse/o11_backend/controller/JobController.java
@@ -3,8 +3,11 @@ package lk.ijse.gdse.o11_backend.controller;
 import lk.ijse.gdse.o11_backend.dto.JobDTO;
 import lk.ijse.gdse.o11_backend.entity.Job;
 import lk.ijse.gdse.o11_backend.service.impl.JobServiceImpl;
+import lk.ijse.gdse.o11_backend.util.APIResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,41 +28,54 @@ public class JobController {
     }*/
 
     @PostMapping("create")
-    public void createJob(@RequestBody JobDTO jobDTO){
+    public ResponseEntity<APIResponse> createJob(@RequestBody JobDTO jobDTO){
+       /*jobService.saveJob(jobDTO);
+       return ResponseEntity.ok(new APIResponse(200,"Success", "Job created successfully"));*/
+
+        // mehma use krnnth puluwn status eka wenama dala ok,created,accepted,bad_request,bad_gateway etc....
+        // new keyword eka use krta kmk naa methana...
         jobService.saveJob(jobDTO);
+        return new ResponseEntity(new APIResponse(200, "Success", "Job created successfully"), HttpStatus.OK);
+
     }
 
     @GetMapping("get/{id}")
-    public JobDTO getJob(@PathVariable String id){
-        return jobService.getJob(id);
+    public ResponseEntity<APIResponse> getJob(@PathVariable String id){
+        JobDTO jobDTO = jobService.getJob(id);
+        return ResponseEntity.ok(new APIResponse(200,"Success", jobDTO));
     }
 
     @PutMapping("update")
-    public void updateJob(@RequestBody JobDTO jobDTO){
+    public ResponseEntity<APIResponse> updateJob(@RequestBody JobDTO jobDTO){
         jobService.updateJob(jobDTO);
+        return ResponseEntity.ok(new APIResponse(200,"Success", "Job updated successfully"));
     }
 
     @GetMapping("all")
-    public List<JobDTO> getAllJobs(){
-        return jobService.getAllJobs();
+    public ResponseEntity<APIResponse> getAllJobs(){
+        List<JobDTO> jobs = jobService.getAllJobs();
+        return ResponseEntity.ok(new APIResponse(200,"Success", jobs));
     }
 
     @PatchMapping("status/{id}")
-    public void changeStatus(@PathVariable String id){
+    public ResponseEntity<APIResponse> changeStatus(@PathVariable String id){
         jobService.changeJobStatus(id);
+        return ResponseEntity.ok(new APIResponse(200,"Success", "Job status changed successfully"));
     }
 
     @GetMapping("search/{keyword}")
-    public List<Job> searchJob(@PathVariable String keyword) {
-        return jobService.searchJob(keyword);
+    public ResponseEntity<APIResponse> searchJob(@PathVariable String keyword) {
+        List<Job> jobs = jobService.searchJob(keyword);
+        return ResponseEntity.ok(new APIResponse(200, "Success", jobs));
     }
 
     @GetMapping
-    public Page<Job> getAllJobs(
+    public ResponseEntity<APIResponse> getAllJobs(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size
     ) {
-        return jobService.getJobs(page, size);
+        Page<Job> jobPage = jobService.getJobs(page, size);
+        return ResponseEntity.ok(new APIResponse(200, "Success", jobPage));
     }
 
 }
---
+++ SPRING/Spring Framework/11_Backend/src/main/java/lk/ijse/gdse/o11_backend/util/APIResponse.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse.o11_backend.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class APIResponse {
+    private int status;
+    private String message;
+    private Object data;
+}
