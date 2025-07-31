0c92d0fd6ee03dbe78efa35d29cba006105b0016
sasuniii0 <sasuniwijerathne@gmail.com>
2025-07-31 14:21:33 +0530

    Spring security configuration - auth service done

---
+++ SPRING/Spring Framework/13_Auth_BN/src/main/java/lk/ijse/gdse/o13_auth_bn/service/AuthService.java
@@ -0,0 +1,29 @@
+package lk.ijse.gdse.o13_auth_bn.service;
+
+import lk.ijse.gdse.o13_auth_bn.dto.AuthDTO;
+import lk.ijse.gdse.o13_auth_bn.dto.AuthResponseDTO;
+import lk.ijse.gdse.o13_auth_bn.entity.User;
+import lk.ijse.gdse.o13_auth_bn.repository.UserRepository;
+import lk.ijse.gdse.o13_auth_bn.util.JWTUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTUtil jwtUtil;
+
+    public AuthResponseDTO authenticate(AuthDTO authDTO) {
+        User user = userRepository.findByUsername(authDTO.getUserName()).orElseThrow(() -> new RuntimeException("User not found"));
+        if (!passwordEncoder.matches(authDTO.getPassword(), user.getPassword())) {
+            throw new BadCredentialsException("Invalid Credentials");
+        }
+
+        String token = jwtUtil.generateToken(authDTO.getUserName());
+        return new AuthResponseDTO(token);
+    }
+}
