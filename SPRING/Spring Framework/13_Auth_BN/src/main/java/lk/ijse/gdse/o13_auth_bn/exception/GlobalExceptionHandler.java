65c138af25b8bbafcfd23f51f2a417e1dc2aba25
sasuniii0 <sasuniwijerathne@gmail.com>
2025-07-31 14:38:23 +0530

    handle exception classes

--- SPRING/Spring Framework/13_Auth_BN/src/main/java/lk/ijse/gdse/o13_auth_bn/exception/GlobalExceptionHandler.java
+++ SPRING/Spring Framework/13_Auth_BN/src/main/java/lk/ijse/gdse/o13_auth_bn/exception/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package lk.ijse.gdse.o13_auth_bn.exception;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import lk.ijse.gdse.o13_auth_bn.dto.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -11,5 +15,23 @@ public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<ApiResponse> handleGenericException(Exception e) {
         return new ResponseEntity<>(new ApiResponse(500, "Internal Server Error", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ApiResponse> handleUsernameNotFoundException() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(404, "User not found", null));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<ApiResponse> handleBadCredentialsException(BadCredentialsException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiResponse(401, "Bad credentials", e.getMessage()));
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ApiResponse> handleExpiredJwtException(ExpiredJwtException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(401, "Token expired", e.getMessage()));
     }
 }
