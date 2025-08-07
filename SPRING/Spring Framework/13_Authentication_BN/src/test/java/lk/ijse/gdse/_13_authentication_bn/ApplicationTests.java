3a0487b67dc8bfef886c1c970ada6f3b22137c61
sasuniii0 <sasuniwijerathne@gmail.com>
2025-08-07 12:09:07 +0530

    Add initial project structure with Maven wrapper, application properties, and test classes

---
+++ SPRING/Spring Framework/13_Authentication_BN/src/test/java/lk/ijse/gdse/_13_authentication_bn/ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse._13_authentication_bn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ SPRING/Spring Framework/14_Spring_Testing/src/main/java/lk/ijse/gdse/o14_spring_testing/Application.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.o14_spring_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
